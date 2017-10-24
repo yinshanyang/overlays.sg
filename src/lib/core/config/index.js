900386c1ca164b71ae809a92dea5eedd0a735755
Yin Shanyang <me@yinshanyang.com>
2017-10-24 13:12:39 +0800

    meow

--- src/lib/core/config/index.js
+++ src/lib/core/config/index.js
@@ -2,11 +2,12 @@ export default {
   map: {
     apiKey: 'pk.eyJ1IjoieWluc2hhbnlhbmciLCJhIjoiNDM4MDVlOWFjNDkwNzQwZTlmYTIzNDlmZGUzMzRlMTkifQ.9TdM9ymU2sqMKrRLYCvllQ',
     center: [103.834534, 1.316688],
-    fit: [[103.596954345, 1.23312012479], [104.049453735, 1.47887018872]],
+    // fit: [[103.596954345, 1.23312012479], [104.049453735, 1.47887018872]],
+    fit: [[103.5444259643555, 1.1936468621802765], [104.08241271972659, 1.4847047237894748]], // tighter fit, just mainland singapore
     zoom: 13,
     minZoom: 10,
     maxZoom: 16,
-    bbox: [[103.460999, 1.0834620335045826], [104.208069, 1.6024216765509463]],
+    bbox: [[103.5485458374024, 1.2101227287239942], [104.0865325927735, 1.5011786218272152]],
     // mapStyle: 'mapbox://styles/yinshanyang/cj4fqx67g0h1t2spe41e0si37'
     // mapStyle: 'mapbox://styles/yinshanyang/cj6g4ikbs33v82rmoq0kak4eb' // Dark-Clean
     mapStyle: 'mapbox://styles/yinshanyang/cj6vloip6248d2speg4su1zmv' // Dark-Clean-NoLabels
--- src/lib/overlays/isochrone/components/Overlay/index.js
+++ src/lib/overlays/isochrone/components/Overlay/index.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types'
 import classNames from './styles.css'
 
 import GeoJSONOverlay from '../../../../core/components/common/GeoJSONOverlay'
-import stations from '../../data/stations.geo.json'
-import line from '../../data/line.geo.json'
+// import stations from '../../data/stations.geo.json'
+// import query from '../../data/query.geo.json'
+// import line from '../../data/line.geo.json'
+import mask from '../../data/mask.geo.json'
 
 // green
 // const colors = ['#343332', '#34573c', '#407a55', '#5b9b78', '#81bca0', '#b2dbcd', '#edf8fb']
@@ -23,8 +25,8 @@ import line from '../../data/line.geo.json'
 // const colors = ['#343332', '#6a416e', '#866094', '#9d84b3', '#b4abce', '#cdd1e6', '#edf8fb']
 
 // purple-orange
-const colors = ['#343332', '#7a3d56', '#a65960', '#cc7c6a', '#e9a47a', '#fdcf99', '#ffffd4']
-// const colors = ['#343332', '#773b5a', '#a4536b', '#c9757a', '#e69a8c', '#fac2a8', '#fef0d9']
+// const colors = ['#343332', '#7a3d56', '#a65960', '#cc7c6a', '#e9a47a', '#fdcf99', '#ffffd4']
+const colors = ['#343332', '#773b5a', '#a4536b', '#c9757a', '#e69a8c', '#fac2a8', '#fef0d9']
 
 // orange,  flames, everything is burning
 // const colors = ['#343332', '#774325', '#a65d2a', '#cd7f3d', '#eba55e', '#fed08f', '#ffffd4']
@@ -63,6 +65,13 @@ const configs = {
       'circle-stroke-color': '#343332'
     }
   },
+  mask: {
+    id: 'mask',
+    type: 'fill',
+    paint: {
+      'fill-color': '#191a1a'
+    }
+  },
   contours: {
     id: 'contours',
     before: 'water',
@@ -83,9 +92,9 @@ const configs = {
   }
 }
 
-const Legend = () => (
+const Legend = ({ label }) => ( // eslint-disable-line
   <div className={classNames.areas} style={{background: '#343332', color: '#fff', width: 232}}>
-    <label style={{display: 'block', paddingBottom: 8, opacity: 0.7, clear: 'both'}}>Time delay in minutes:</label>
+    <label style={{display: 'block', paddingBottom: 8, opacity: 0.7, clear: 'both'}}>Originating from <strong>{label}</strong>,<br />Time delay in minutes:</label>
     {
       colors
         .filter((_, index) => index > 0)
@@ -112,7 +121,7 @@ class Overlay extends PureComponent {
 
   render () {
     const { map, data } = this.props
-    const selection = 2
+    // const selection = 20
     // const values = data.features
     //   .map((feature) => +feature.properties.value)
     //   .filter((d) => d > 0)
@@ -126,6 +135,7 @@ class Overlay extends PureComponent {
 
     // const max = values[values.length - 1]
     // const max = values[~~(values.length * 0.995)]
+    // const max = 300 // max value for delta of all station breakdowns
     const max = 1800 // max value for IMPACT visualization
 
     // continuous heatmap
@@ -143,12 +153,14 @@ class Overlay extends PureComponent {
     return (
       <div>
         <GeoJSONOverlay map={map} data={data} {...configs.contours} />
-        <GeoJSONOverlay map={map} data={line} {...configs.line} />
-        <GeoJSONOverlay map={map} data={stations} {...configs.stations} />
-        <GeoJSONOverlay map={map} data={stations.features[selection]} {...configs.selection} />
+        <GeoJSONOverlay map={map} data={mask} {...configs.mask} />
         <Legend />
       </div>
     )
+    // <GeoJSONOverlay map={map} data={line} {...configs.line} />
+    // <GeoJSONOverlay map={map} data={stations} {...configs.stations} />
+    // <GeoJSONOverlay map={map} data={query.features[selection]} {...configs.selection} />
+    // <Legend label={query.features[selection].properties.name} />
   }
 
   handleChange = ({ location }) => {
